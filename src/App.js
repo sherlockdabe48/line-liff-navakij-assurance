978da0fa06cd3d050fd9f09f1ed6cffa13b6ff75
sherlockdabe48 <sherlockdabe48@gmail.com>
2022-02-23 00:54:09 +0700

    initLine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import liff from '@line/liff'
+import logo from './logo.svg'
+import { useEffect, useState } from 'react';
 
-function App() {
+
+function App () {
+
+  const [pictureUrl, setPictureUrl] = useState(logo)
+  const [idToken, setIdToken] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [statusMessage, setStatusMessage] = useState('')
+  const [userId, setUserId] = useState('')
+
+  const initLine = () => {
+    liff.init({ liffId: '1656915926-LAjMq7EB' }, () => {
+      if (liff.isLoggedIn()) runApp() 
+      else liff.login()
+    }, err => console.error(err))
+  }
+
+  const runApp = () => {
+    const idToken = liff.getIDToken()
+    this.idToken = idToken
+    liff.getProfile().then(profile => {
+      console.log(profile)
+      setDisplayName(profile.displayName)
+      setPictureUrl(profile.pictureUrl)
+      setStatusMessage(profile.statusMessage)
+      setUserId(profile.userId)
+    }).catch(err => console.error(err));
+  }
+
+  const logout = () => { 
+    liff.logout();
+    window.location.reload()
+  }
+
+  useEffect(() => {
+    initLine()
+  }, [])
+
   return (
-    <>Navakij</>
+    <div className='App'>
+      <div className='App-header'>
+        <div style={{ textAlign: 'center' }}>
+          <h1>Navakij Assurance</h1>
+          <hr/>
+          <img src={pictureUrl} alt="user" width="300px" height="300px" />
+          <p style={{ textAlign: 'left', marginLeft: '20%', marginRight: '20%' }}><b>id token: </b> { idToken }</p>
+          <p style={{ textAlign: 'left', marginLeft: '20%', marginRight: '20%' }}><b>display name: </b> { displayName }</p>
+          <p style={{ textAlign: 'left', marginLeft: '20%', marginRight: '20%' }}><b>status message: </b> { statusMessage }</p>
+          <p style={{ textAlign: 'left', marginLeft: '20%', marginRight: '20%' }}><b>user id: </b>{ userId }</p>
+          <button onClick={() => logout()} style={{ width: '100%', height: '30px' }}>Logout</button>
+        </div>
+      </div>
+    </div>
   );
 }
 

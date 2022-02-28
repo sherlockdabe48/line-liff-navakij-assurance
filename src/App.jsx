2c4971e9e258e51e39fe81db4adfcf5020444f10
sherlockdabe48 <sherlockdabe48@gmail.com>
2022-02-28 22:32:39 +0700

    add function pushClaimMessageMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./styles/App.scss";
 import React from "react";
+import axios from "axios";
 
 // import { Navigate, Routes, Route,  } from "react-router-dom";
 import { useLocation } from "react-router-dom";
@@ -60,9 +61,96 @@ function App() {
     window.location.reload();
   };
 
+  const pushClaimMessageMenu = async () => {
+    const token =
+      "Wy1Zsg1vsejOrMQFpybSvM/OMbYvMmyaf4iYxsHOxexJcVKs1+4uZP+jcRcJSDUkstjVtcFRyst1Hq3WyAgFiNnZ4WiVJHCJpKGG8PbmahPk+t8IUTuxvyBNkyNRIBCY6EvOaAaA6sFirOtTEIY91gdB04t89/1O/w1cDnyilFU=";
+    try {
+      await axios.post("https://api.line.me/v2/bot/message/push", {
+        headers: { Authorization: `Bearer ${token}` },
+        body: {
+          to: "U39b4954db3c1a4e88fd9d14c0b210e0a",
+          messages: [
+            {
+              type: "flex",
+              altText: "ผลิตภัณฑ์ประกันภัยและสินค้า",
+              contents: {
+                type: "carousel",
+                contents: [
+                  {
+                    type: "bubble",
+                    direction: "ltr",
+                    hero: {
+                      type: "image",
+                      url: "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
+                      size: "full",
+                      aspectRatio: "1.51:1",
+                      aspectMode: "fit",
+                      action: {
+                        type: "uri",
+                        uri: "https://www.navakij.co.th/th/products/motor-insurance",
+                      },
+                    },
+                  },
+                  {
+                    type: "bubble",
+                    direction: "ltr",
+                    hero: {
+                      type: "image",
+                      url: "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
+                      size: "full",
+                      aspectRatio: "1.51:1",
+                      aspectMode: "fit",
+                      action: {
+                        type: "uri",
+                        uri: "https://www.navakij.co.th/th/products/miscellaneous-insurance",
+                      },
+                    },
+                  },
+                  {
+                    type: "bubble",
+                    direction: "ltr",
+                    hero: {
+                      type: "image",
+                      url: "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
+                      size: "full",
+                      aspectRatio: "1.51:1",
+                      aspectMode: "fit",
+                      action: {
+                        type: "uri",
+                        uri: "https://www.navakij.co.th/th/products/property-insurance",
+                      },
+                    },
+                  },
+                  {
+                    type: "bubble",
+                    direction: "ltr",
+                    hero: {
+                      type: "image",
+                      url: "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
+                      size: "full",
+                      aspectRatio: "1.51:1",
+                      aspectMode: "fit",
+                      action: {
+                        type: "uri",
+                        uri: "https://www.navakij.co.th/th/products/marine--transportation-insurance",
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      });
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
+
   useEffect(() => {
     initLine();
-  }, [initLine]);
+    // if (query.get("products")) pushClaimMessageMenu();
+  }, [initLine, query]);
 
   return (
     <div className="App">

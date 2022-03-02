f0ea8e9189f67c8d5106c007da9a6837bd5e7357
sherlockdabe48 <sherlockdabe48@gmail.com>
2022-03-03 01:58:58 +0700

    get termsConditionsHTML

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ import { useEffect, useState } from "react"
 import liff from "@line/liff"
 
 function App() {
+  const apiPath = "https://uat-web.navakij.co.th/consentmanager-api-1.0.0/"
+  const headers = {
+    CONTROLKEY:
+      "663FA29A4761FFEB89DC56944333A28296014D21F09BAF92A6F3027C99C61F2E",
+    Authorization: "Bearer 32f452ff-41fb-50cd-9b13-156e56b84880",
+    "Content-Type": "application/json",
+  }
   // function useQuery() {
   //   const { search } = useLocation();
   //   return React.useMemo(() => new URLSearchParams(search), [search]);
@@ -44,7 +51,10 @@ function App() {
       <Header />
       {/* <div>profile: {profile}</div> */}
       <Routes>
-        <Route path="/terms-conditions" element={<TermsAndCondition />} />
+        <Route
+          path="/terms-conditions"
+          element={<TermsAndCondition apiPath={apiPath} headers={headers} />}
+        />
         <Route path="/verify-identity" element={<VerifyIdentity />} />
         <Route path="*" element={<Navigate to="/terms-conditions" replace />} />
       </Routes>
--- src/components/TermsAndCondition.jsx
+++ src/components/TermsAndCondition.jsx
@@ -2,27 +2,52 @@ import axios from "axios"
 import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-export default function TermsAndCondition() {
+export default function TermsAndCondition({ headers, apiPath }) {
   const navigate = useNavigate()
 
   const [isConsent, setIsConsent] = useState(false)
+  const [termsConditionsHTML, setTermsConditionsHTML] = useState("")
 
   const getIsConsent = () => {
     // const res = await axios.get("")
     return false
   }
 
+  const getTermsConditions = async () => {
+    const { data } = await axios.get(
+      `${apiPath}consent/getmasterconsent/`,
+      {
+        masterConsentCode: "MC-LINEOA-001",
+        system: "LINEOA",
+        project: "LINEOA-001",
+      },
+      { headers: headers }
+    )
+    setTermsConditionsHTML(data.masterConsent?.consentBodyHtmlText)
+  }
+
   useEffect(() => {
-    const checkIsConsent = () => {
-      if (isConsent) navigate("/verify-identity")
-    }
-    setIsConsent(getIsConsent())
-    checkIsConsent()
+    ;(async () => {
+      const { data } = await axios.get(
+        `${apiPath}consent/checkisconsent`,
+        {
+          masterConsentCode: "MC-LINEOA-001",
+          system: "LINEOA",
+          project: "LINEOA-001",
+          identityKey: "sherlock48",
+        },
+        { headers: headers }
+      )
+      setIsConsent(data.isConsent)
+    })()
+
+    if (isConsent) navigate("/verify-identity")
+    else getTermsConditions()
   }, [isConsent, navigate])
 
   return (
     <div className="terms-conditions">
-      <div className="terms-conditions-container"></div>
+      <div className="terms-conditions-container">{termsConditionsHTML}</div>
       <div className="input-button-wrapper">
         <label className="input-wrapper">
           <input type="checkbox" name="accept-terms-conditions" />

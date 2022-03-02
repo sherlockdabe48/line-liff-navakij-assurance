5c12af818aa7172fc1fa4f0239bf32cd0cf51b9d
sherlockdabe48 <sherlockdabe48@gmail.com>
2022-03-02 23:49:12 +0700

    edit initLine

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import "./styles/App.scss";
-import React from "react";
-import axios from "axios";
+import "./styles/App.scss"
+import React from "react"
+import axios from "axios"
 
-import { Navigate, Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom"
 // import { useLocation } from "react-router-dom";
-import Header from "./components/Header";
-import TermsAndCondition from "./components/TermsAndCondition";
-import VerifyIdentity from "./components/VerifyIdentity";
-import { useEffect, useState } from "react";
-import liff from "@line/liff";
+import Header from "./components/Header"
+import TermsAndCondition from "./components/TermsAndCondition"
+import VerifyIdentity from "./components/VerifyIdentity"
+import { useEffect, useState } from "react"
+import liff from "@line/liff"
 
 function App() {
   // function useQuery() {
@@ -18,37 +18,30 @@ function App() {
   // const query = useQuery();
 
   // const [pictureUrl, setPictureUrl] = useState("");
-  const [idToken, setIdToken] = useState("");
+  const [idToken, setIdToken] = useState("")
   // const [displayName, setDisplayName] = useState("");
   // const [statusMessage, setStatusMessage] = useState("");
   // const [userId, setUserId] = useState("");
-  const [profile, setProfile] = useState(null);
+  const [profile, setProfile] = useState(null)
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const initLine = async () => {
-    await liff.init(
-      { liffId: "1656915926-p1LyQKPo" },
-      () => {
-        if (liff.isLoggedIn()) runApp();
-        else liff.login();
-      },
-      (err) => console.error(err)
-    );
-  };
+    await liff.init({ liffId: "1656915926-p1LyQKPo" })
+  }
 
   const runApp = () => {
     const idToken = liff.getIDToken().then(() => {
-      setIdToken(idToken);
-    });
+      setIdToken(idToken)
+    })
     liff.getProfile().then((profile) => {
-      console.log(profile);
-      setProfile(profile);
-    });
-  };
+      console.log(profile)
+      setProfile(profile)
+    })
+  }
 
   useEffect(() => {
-    initLine();
-  }, []);
+    initLine()
+  }, [])
 
   return (
     <div className="App">
@@ -60,7 +53,7 @@ function App() {
         <Route path="*" element={<Navigate to="/terms-conditions" replace />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

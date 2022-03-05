cd69192f9ff018e979916fb43ee43ecd2f2dd209
sherlockdabe48 <sherlockdabe48@gmail.com>
2022-03-05 21:00:04 +0700

    OtpConfirmPopup

--- src/components/OTPConfirmPopup.jsx
+++ src/components/OTPConfirmPopup.jsx
@@ -1,10 +1,73 @@
-import React from "react";
-import { Close } from "@material-ui/icons";
+import React, { useEffect, useState } from "react"
+import { Close, Refresh } from "@material-ui/icons"
+// import RefreshIcon from "@mui/icons-material/Refresh"
 
-export default function OTPConfirmPopup() {
+export default function OTPConfirmPopup(props) {
+  const { otpRefData, phoneNumber, handleClose, onSubmitOtp } = props
+
+  const [otpDigits, setOtpDigits] = useState(["", "", "", "", "", ""])
+  const [otpString, setOtpString] = useState("")
+
+  function handleInputOtp(e, digitStr, index) {
+    let currentOtpDigits = [...otpDigits]
+    currentOtpDigits[index] = e.target.value
+    setOtpDigits(currentOtpDigits)
+  }
+
+  function handleSubmit() {
+    if (otpDigits.includes("")) {
+      console.log("you have white space")
+      return
+    }
+    const stringWithComma = otpDigits.toString()
+    const stringNoComma = stringWithComma.replaceAll(",", "")
+    setOtpString(stringNoComma)
+  }
+
+  useEffect(() => {
+    if (otpString) {
+      onSubmitOtp(otpString)
+    }
+  }, [otpString])
+
   return (
-    <div className="otp-confirm-popup">
-      <Close className="close-icon" />
+    <div className="modal-container">
+      <div className="otp-confirm-popup">
+        <Close className="close-icon" onClick={handleClose} />
+        <div className="wrapper flex flex-column">
+          <h3>ยืนยันรหัส OTP</h3>
+          <div className="text-desc-wrapper">
+            <p>กรุณากรอกรหัส OTP 6 หลัก</p>
+            <p>
+              ที่ส่งไปยังเบอร์โทรศัพท์ {phoneNumber} (Ref: {otpRefData.otpRef})
+            </p>
+          </div>
+          <div className="flex otp-input-wrapper">
+            {otpDigits.map((digitStr, index) => {
+              return (
+                <input
+                  className="one-digit-input text-center"
+                  type="text"
+                  key={index}
+                  maxLength={1}
+                  value={otpDigits[index]}
+                  onChange={(e) => handleInputOtp(e, digitStr, index)}
+                />
+              )
+            })}
+          </div>
+          <div className="refresh">
+            <Refresh />
+            <span>ส่งรหัส OTP ได้อีกครั้งใน 04.50 นาที</span>
+          </div>
+          <button
+            className="btn btn-primary m-l-40 m-r-40"
+            onClick={handleSubmit}
+          >
+            ยืนยัน
+          </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
--- src/components/VerifyIdentity.jsx
+++ src/components/VerifyIdentity.jsx
@@ -1,14 +1,92 @@
-import React from "react";
-import OTPConfirmPopup from "./OTPConfirmPopup";
+import axios from "axios"
+import React, { useState } from "react"
+import OTPConfirmPopup from "./OTPConfirmPopup"
 
 export default function VerifyIdentity() {
-  const handleSubmit = () => {
-    console.log("form");
-  };
+  const [isFormValid, setIsFormValid] = useState(false)
+  const [idPassport, setIdPassport] = useState("")
+  const [birthDate, setBirthDate] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [form, setForm] = useState({})
+  const [showOtpPopup, setShowOtpPopup] = useState(false)
+  const [otpRefData, setOtpRefData] = useState(null)
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    console.log("form: ", form)
+    try {
+      // const { data } = await axios.post('https://uat-web.navakij.co.th/myinformation-api-1.0.0/api/customer/otp/request', {}, { headers })
+      const data = {
+        msgCode: "SUCCESS",
+        msgDescription: "",
+        msgContent: "",
+        data: {
+          optRef: "Brw8zh",
+          otpExpiredOnString: "2022-02-25 13:39:26",
+        },
+      }
+      if (data.msgCode === "SUCCESS") {
+        console.log(data.data)
+        console.log(otpRefData)
+        setOtpRefData(data.data)
+        setShowOtpPopup(true)
+      }
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  }
+
+  function checkIsFormValid() {
+    if (
+      idPassport.length === 13 &&
+      birthDate.length === 10 &&
+      phoneNumber.length >= 10
+    ) {
+      setIsFormValid(true)
+    } else setIsFormValid(false)
+    // if (!idPassport.length === 13) setIsFormValid(false)
+    // else if (!birthDate) setIsFormValid(false)
+    // else if (!phoneNumber === 10) setIsFormValid(false)
+  }
+
+  function handleChange(e, type) {
+    if (type === "id") {
+      setIdPassport(e.target.value)
+    }
+    if (type === "birthDate") {
+      setBirthDate(e.target.value)
+    }
+    if (type === "phoneNumber") {
+      setPhoneNumber(e.target.value)
+    }
+
+    setForm({
+      identityValue: idPassport,
+      dateOfBirthString: birthDate,
+      mobileNo: phoneNumber,
+    })
+
+    checkIsFormValid()
+  }
+
+  function handleCloseOtpPopup() {
+    setShowOtpPopup(false)
+  }
+
+  function onSubmitOtp(otpString) {
+    console.log(otpString)
+    handleCloseOtpPopup()
+  }
 
   return (
     <div className="verify-identity">
-      <h3>การยืนยันตัวตน</h3>
+      <h3 className="m-t-16 m-b-16">การยืนยันตัวตน</h3>
+      {/* Valid: {isFormValid ? "Yes" : "No"} <br />
+      {idPassport.length}
+      <br />
+      {birthDate.length}
+      <br />
+      {phoneNumber.length} */}
       <form className="verify-identity-form" onSubmit={handleSubmit}>
         <div className="form-item">
           <label className="form-label" htmlFor="id-passport">
@@ -18,27 +96,57 @@ export default function VerifyIdentity() {
             className="input-item"
             type="text"
             name="id-passport"
+            minLength={13}
             maxLength={13}
+            value={idPassport}
+            onChange={(e) => handleChange(e, "id")}
           />
         </div>
         <div className="form-item">
           <label className="form-label" htmlFor="birth-date">
             วัน/เดือน/ปีเกิด*
           </label>
-          <input className="input-item" type="date" name="birth-date" />
+          <input
+            className="input-item"
+            type="date"
+            name="birth-date"
+            value={birthDate}
+            onChange={(e) => handleChange(e, "birthDate")}
+          />
         </div>
         <div className="form-item">
           <label className="form-label" htmlFor="phone-number">
             เบอร์โทรศัพท์*
           </label>
-          <input className="input-item" type="number" name="phone-number" />
+          <input
+            className="input-item"
+            type="text"
+            name="phone-number"
+            maxLength={10}
+            value={phoneNumber}
+            onChange={(e) => handleChange(e, "phoneNumber")}
+          />
         </div>
-        <button className="btn btn-primary verify-button" type="submit">
+        <button
+          className={`btn btn-primary verify-button ${
+            isFormValid ? "" : "btn-disabled"
+          }`}
+          type="submit"
+          disabled={!isFormValid}
+        >
           ตรวจสอบ
         </button>
       </form>
-
-      <OTPConfirmPopup />
+      {otpRefData?.optRef
+        ? showOtpPopup && (
+            <OTPConfirmPopup
+              phoneNumber={phoneNumber}
+              otpRefData={otpRefData}
+              handleClose={handleCloseOtpPopup}
+              onSubmitOtp={onSubmitOtp}
+            />
+          )
+        : showOtpPopup && "Loading..."}
     </div>
-  );
+  )
 }

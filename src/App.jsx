642a545c32574554e7981e0707dcb15f3b90da57
sherlockdabe48 <sherlockdabe48@gmail.com>
2022-03-25 00:44:16 +0700

    fix deployment

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App({
   // LINE LIFF FUNCTIONS
   // Function runLiff จะเป็นการยิง API ไปหา Line เพื่อ initiate ก่อนที่จะข้อใช้ข้อมูลของ Line User
   async function runLiff() {
-    // liffId ได้มาจาก console ของ line developer > Provider > Chanel > Liff Id
+    // liffId ได้มาจาก console ของ line developer > Provider > Chanel > Liff Id "1656990746-QbJoG5ny"
     await liff.init({ liffId: "1656990746-QbJoG5ny" }).catch((err) => {
       throw err
     })
